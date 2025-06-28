0165d0f908d46ba5940347942d363b1c1be9257e
jjamesliu <liujameszhi@gmail.com>
2025-06-28 11:33:57 -0700

    beginning screen and styling finished

---
+++ README.md
@@ -0,0 +1,10 @@
+# Reduce Carbon (CO2)
+Check your carbon footprint.
+
+## Tech Stack
+Build Tool: Vite<br/>
+Library: React<br/>
+Styling: TailwindCSS<br/>
+Deployment: Netlify<<br/>
+
+APIS: AutoClimatiq
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import './Header.css'
+
+export default function Header() {
+    return (
+    <div className='flex flex-col justify-center items-center h-screen w-screen'>
+        <h1 className='font-bold text-5xl text-center fade-in-down'>Help Reduce Carbon Emissions</h1>
+    </div>
+    )
+}
